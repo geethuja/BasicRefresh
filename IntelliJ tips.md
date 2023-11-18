94facd9f3e074579e67ed6c71aded901f0bb9ada
geethujatom <geethuja@gmail.com>
2023-11-18 17:09:24 +0100

    method codes are added

--- IntelliJ tips.md
+++ IntelliJ tips.md
@@ -5,3 +5,10 @@
 
 ### Comment unwanted code 
  - select code , code > command code with line comment / comment code with block comment
+
+### Tips 
+- search with tips keyword to now about tips
+
+### Shortcuts 
+- type psvm and hit enter : get main method in Java
+- 
--- src/CodeChallenge.java
+++ src/CodeChallenge.java
@@ -5,7 +5,7 @@ public class CodeChallenge {
         int score = 5000;
         int levelCompleted = 5;
         int bonus = 100;
-
+      /*
         int finalScore = score;
 
         if (gameOver == true) {
@@ -13,6 +13,10 @@ public class CodeChallenge {
             System.out.println("Your final score was "+finalScore);
 
         }
+        above code is replaced with function: calculateScore();
+        */
+
+        calculateScore(gameOver,score,levelCompleted,bonus);
 
   /*    Rewrite the same code about without changing code ;
   1 way copy code and create new variable by adding new to old variable name
@@ -29,11 +33,9 @@ public class CodeChallenge {
             System.out.println("Your final score was "+newFinalScore);
         }
 
-        */
-
-//          Below code used memory more efficiency than above solution ;
-//          But remember this is a duplicate code
-//          it can be solved by using methods
+        Below code used memory more efficiency than above solution ;
+          But remember this is a duplicate code
+         it can be solved by using methods
 
         score = 8000;
         levelCompleted = 8;
@@ -45,7 +47,22 @@ public class CodeChallenge {
             finalScore += (levelCompleted * bonus);
             System.out.println("Your final score was "+finalScore);
 
-        }
+        }*/
+        score = 8000;
+        levelCompleted = 8;
+        bonus = 300;
 
+        calculateScore(gameOver,score,levelCompleted,bonus);
+    }
+
+    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+
+        int finalScore = score;
+
+        if (gameOver == true) {
+            finalScore += (levelCompleted * bonus);
+            System.out.println("Your final score was " + finalScore);
+
+        }
     }
 }
